f8dbb4ab4bb6e6d2a10fb9a79561a0ab9abc015d
duclapjp <duclapjp@gmail.com>
2022-01-15 12:21:42 +0700

    update hien thi mon an o trang chu

--- src/app/component/home/home-page/home-page.component.ts
+++ src/app/component/home/home-page/home-page.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TokenService} from "../../../service/token.service";
 import {Router} from "@angular/router";
 import {UserService} from "../../../service/user.service";
 import {User} from "../../../model/User";
+import {FoodService} from "../../../service/food.service";
+import {Food} from "../../../model/food";
+import {FoodOrderService} from "../../../service/foodOrder.service";
 
 @Component({
   selector: 'app-home-page',
@@ -15,13 +18,21 @@ export class HomePageComponent implements OnInit {
   userName = '';
   userMoney = 0;
   // @ts-ignore
-  user: User ={}
+  user: User = {}
+  foods: Food [] = [];
+  foodId: number = 0;
+  // @ts-ignore
+  food: Food = {}
 
   constructor(
     private tokenService: TokenService,
     private router: Router,
-    private userService: UserService
-  ) { }
+    private userService: UserService,
+    private foodService: FoodService,
+    private foodOrderService: FoodOrderService
+  ) {
+    this.showAllFood();
+  }
 
   ngOnInit(): void {
     this.check();
@@ -31,13 +42,15 @@ export class HomePageComponent implements OnInit {
 
   check() {
     const token = this.tokenService.getToken();
-    if (token!=null){
+    if (token != null) {
       this.checkLogin = true;
     }
   }
+
   getUserName() {
     this.userName = this.tokenService.getName();
   }
+
   getUserById() {
 
     const userId = this.tokenService.getUserId();
@@ -50,7 +63,26 @@ export class HomePageComponent implements OnInit {
   logout() {
     this.tokenService.logout();
     this.router.navigate(['/']);
-    this.checkLogin =false
+    this.checkLogin = false
+  }
 
+  showAllFood() {
+    this.foodService.findAll().subscribe(foods => {
+      this.foods = foods;
+    });
+  }
+
+
+  addShopCart($event: MouseEvent) {
+    // @ts-ignore
+    this.foodId = event.target.id
+    //lấy userId
+    //tìm trong bảng foodOrder với userId. check status nếu chưa thanh toán
+    //nếu có status chưa thanh toán thì lấy id của foodOrder lưu vào bảng foodOrderItem với: foodId+ foodOrderId
+    //nếu không có status chưa hoàn thành thì: tạo mới 1 hàng trong foodOrder và lấy id của foodOrder.
+    // tiếp tục lấy id của foodOrder lưu vào bảng foodOrderItem với: foodId+ foodOrderId
+    this.foodService.findById(this.foodId).subscribe(food => {
+      this.food = food;
+    });
   }
 }
